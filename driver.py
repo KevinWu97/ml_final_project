af39a55310deabba957fddb95f6c954fd4514e11
Chris Gong <chris_gong@yahoo.com>
2019-05-12 17:28:27 -0400

    Added mean squared error calculation for entire batch to driver.py

--- driver.py
+++ driver.py
@@ -38,3 +38,33 @@ batch = np.array(batch.values.astype(np.float).tolist())
 for x, y in zip(batch, y_values):
     prediction = network.predict(np.array([[x_val] for x_val in x]))
     print(f"calculated y value - {prediction} actual y value - {y}")
+    
+y_values = np.array(df[['politicalid']].copy().values.astype(np.float).tolist())
+batch = df[['age', 'sex', 'anchoring1', 'anchoring2', 'anchoring3', 'anchoring4', 'reciprocityus',
+               'reciprocityother', 'allowedforbidden', 'quote', 'totalflagestimations',
+               'totalnoflagtimeestimations', 'IATexpart', \
+               'IATexpmath', 'IATexp.overall', 'totexpmissed', 'artwarm', 'diseaseframinga',
+               'diseaseframingb', 'flagdv1', 'flagdv2', 'flagdv3', 'flagdv4', 'flagdv5', 'flagdv6',
+               'flagdv7', \
+               'flagdv8', 'flagsupplement1', 'flagsupplement2', 'flagsupplement3', 'flagtimeestimate1',
+               'flagtimeestimate2', 'flagtimeestimate3', 'flagtimeestimate4', 'gamblerfallacya', \
+               'gamblerfallacyb', 'iatexplicitart1', 'iatexplicitart2', 'iatexplicitart3',
+               'iatexplicitart4', 'iatexplicitart5', 'iatexplicitart6', 'iatexplicitmath1',
+               'iatexplicitmath2', \
+               'iatexplicitmath3', 'iatexplicitmath4', 'iatexplicitmath5', 'iatexplicitmath6',
+               'imaginedexplicit1', 'imaginedexplicit2', 'imaginedexplicit3', 'imaginedexplicit4', 'major',
+               'mathwarm', 'moneyagea', \
+               'moneyageb', 'moneygendera', 'moneygenderb', 'noflagtimeestimate1', 'noflagtimeestimate2',
+               'noflagtimeestimate3', 'noflagtimeestimate4', 'omdimc3', 'quotea', 'quoteb', \
+               'scalesa', 'scalesb', 'sunkcosta', 'sunkcostb', 'sysjust1', 'sysjust2', 'sysjust3',
+               'sysjust4', 'sysjust5', 'sysjust6', 'sysjust7', 'sysjust8']].copy()
+values = []
+for x, y in zip(batch, y_values):
+    prediction = network.predict(np.array([[x_val] for x_val in x]))
+    values.append((prediction, y))
+# calculate mean squared error
+mse = 0
+for predicted_y, actual_y in values:
+    mse = mse + pow((predicted_y - actual_y), 2)
+mse = mse / df.shape[0]
+print(f"mean squared error - {mse}")
